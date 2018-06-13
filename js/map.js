9465515a494fa7a8756892c369bcc547db75f159
Alex Kachar <alexkachar.github@gmail.com>
2018-06-14 01:29:41 +0300

    Валидация формы - js

--- js/map.js
+++ js/map.js
@@ -256,3 +256,117 @@ map.addEventListener('keydown', onPressEscClose);
 
 fadeInterface();
 setAddress();
+
+// Блок валидации формы
+
+var adFormTitleField = adForm.querySelector('#title');
+var adFormPriceField = adForm.querySelector('#price');
+var adFormTypeField = adForm.querySelector('#type');
+var adFormRoomsSelect = adForm.querySelector('#room_number');
+var adFormCapacitySelect = adForm.querySelector('#capacity');
+var adFormCheckInSelect = adForm.querySelector('#timein');
+var adFormCheckOutSelect = adForm.querySelector('#timeout');
+
+adForm.addEventListener('invalid', function (evt) {
+  evt.target.classList.add('ad-form__input--invalid');
+}, true);
+
+adForm.addEventListener('change', function (evt) {
+  var element = evt.target;
+  if (element.validity.valid && element.classList.contains('invalid-input')) {
+    element.classList.remove('invalid-input');
+  }
+});
+
+adFormTitleField.addEventListener('input', function () {
+  if (adFormTitleField.validity.tooShort) {
+    adFormTitleField.setCustomValidity('Минимальная длина — 30 символов');
+  } else if (adFormTitleField.validity.tooLong) {
+    adFormTitleField.setCustomValidity('Максимальная длина — 100 символов');
+  } else if (adFormTitleField.validity.valueMissing) {
+    adFormTitleField.setCustomValidity('Обязательное поле');
+  } else {
+    adFormTitleField.setCustomValidity('');
+  }
+});
+
+adFormTypeField.addEventListener('change', function () {
+  switch (adFormTypeField.value) {
+    case 'flat':
+      adFormPriceField.min = '1000';
+      adFormPriceField.placeholder = '1000';
+      return;
+    case 'house':
+      adFormPriceField.min = '5000';
+      adFormPriceField.placeholder = '5000';
+      return;
+    case 'palace':
+      adFormPriceField.min = '10000';
+      adFormPriceField.placeholder = '10000';
+      return;
+    default:
+      adFormPriceField.min = '0';
+      adFormPriceField.placeholder = '0';
+      return;
+  }
+});
+
+//  Блок проверки количества комнат и гостей
+
+var disableCapacityOptions = function (num) {
+  for (var i = 0; i < adFormCapacitySelect.children.length; i++) {
+    adFormCapacitySelect.children[i].setAttribute('disabled', '');
+  }
+
+  adFormCapacitySelect.selectedIndex = num;
+};
+
+var addCapacityOption = function (from, to) {
+  for (var i = from; i <= to; i++) {
+    adFormCapacitySelect.children[i].removeAttribute('disabled', '');
+  }
+};
+
+disableCapacityOptions(2);
+
+adFormRoomsSelect.addEventListener('change', function (evt) {
+  var target = evt.target;
+
+  if (!target.selectedIndex) {
+    disableCapacityOptions(2);
+    addCapacityOption(2, 2);
+  }
+
+  if (target.selectedIndex === 1) {
+    disableCapacityOptions(1);
+    addCapacityOption(1, 2);
+  }
+
+  if (target.selectedIndex === 2) {
+    disableCapacityOptions(0);
+    addCapacityOption(0, 2);
+  }
+
+  if (target.selectedIndex === 3) {
+    disableCapacityOptions(3);
+    addCapacityOption(3, 3);
+  }
+});
+
+// Блок проверки времеми въезда/выезда
+
+var changeTime = function (target, value) {
+  target.value = value;
+};
+
+adFormCheckInSelect.addEventListener('change', function () {
+  changeTime(adFormCheckOutSelect, adFormCheckInSelect.value);
+});
+
+adFormCheckOutSelect.addEventListener('change', function () {
+  changeTime(adFormCheckInSelect, adFormCheckOutSelect.value);
+});
+
+addEventListener('load', function () {
+  adForm.reset();
+});
